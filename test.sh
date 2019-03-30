9cdf0abaccc99e01d3e859673946ac4060442377
Beth Griggs <Bethany.Griggs@uk.ibm.com>
2019-03-30 12:12:46 +0000

    remove outdated test suite, rewrite in bash

--- test.sh
+++ test.sh
@@ -1,3 +1,8 @@
+MODULE=$1
+MODULE_VERSION=$2
+MODULE_REPOSITORY=$3
+NODE_VERSION=$4
+
 # Source NVM 
 . ~/.nvm/nvm.sh
 
@@ -10,3 +15,5 @@ git clone --depth 1 -b $MODULE_VERSION $MODULE_REPOSITORY .
 nvm use 10.15.3
 npm install 
 npm test
+
+echo $? 
